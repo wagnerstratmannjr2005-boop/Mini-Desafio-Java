a70683c8dc2d6ba25e6dbf648f92e65957891d63
wagnerstratmann07 <wagnerstratmannjr2005@gmail.com>
2026-03-10 21:45:06 -0300

    Estagio 2
    Adicionado um limite de tentativas, utilizando While

--- src/miniDesafio/Desafio.java
+++ src/miniDesafio/Desafio.java
@@ -1,26 +1,37 @@
 package miniDesafio;
 
 import java.util.Scanner;
+
 public class Desafio {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Digite o Login");
-		String usuario = scanner.nextLine();
-		
-		System.out.println("Digite a senha");
-		String senha = scanner.nextLine();
-		
-		if (usuario.equals("admin") && senha.equals("1234")) {
-			System.out.println("Acesso Permitido");
-			
-		} else {
-			System.out.println("Acesso Negado");
-		}
-		
-		scanner.close();
-			
-	}
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int tentativas = 0;
+
+        while (tentativas < 3) {
+
+            System.out.print("Digite o Login: ");
+            String usuario = scanner.nextLine();
+
+            System.out.print("Digite a senha: ");
+            String senha = scanner.nextLine();
+
+            if (usuario.equals("admin") && senha.equals("1234")) {
+                System.out.println("Acesso Permitido");
+                break;
+            } else {
+                tentativas++;
+                System.out.println("Login ou senha incorreto");
+                System.out.println("Tentativas restantes: " + (3 - tentativas));
+            }
+        }
+
+        if (tentativas == 3) {
+            System.out.println("Sistema bloqueado");
+        }
+
+        scanner.close();
+    }
 }
