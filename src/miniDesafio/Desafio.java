83a70ae49b8efb00a91ef5d32fe017b69d12be7f
wagnerstratmann07 <wagnerstratmannjr2005@gmail.com>
2026-03-10 22:36:08 -0300

    Update - Nova Interface

--- src/miniDesafio/Desafio.java
+++ src/miniDesafio/Desafio.java
@@ -1,32 +1,77 @@
 package miniDesafio;
-import javax.swing.JOptionPane;
 
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Desafio {
 
+    static int tentativas = 0;
+
     public static void main(String[] args) {
 
-        int tentativas = 0;
-        String opcao = JOptionPane.showInputDialog("1 - Login\n2 - Sair");
+        JFrame janela = new JFrame("Sistema de Login");
+        janela.setSize(300,200);
+        janela.setLayout(null);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        while (tentativas < 3) {
+        JLabel labelUsuario = new JLabel("Login:");
+        labelUsuario.setBounds(20,20,80,25);
+        janela.add(labelUsuario);
 
-            String usuario = JOptionPane.showInputDialog("Digite seu Login: ");
-            String senha = JOptionPane.showInputDialog("Digite a senha: ");
+        JTextField campoUsuario = new JTextField();
+        campoUsuario.setBounds(100,20,150,25);
+        janela.add(campoUsuario);
 
-            if (usuario.equals("admin") && senha.equals("1234")) {
-            	JOptionPane.showMessageDialog(null,"Acesso Permitido");
-                break;
-            } else {
-                tentativas++;
-                JOptionPane.showMessageDialog(null, "Login ou senha incorretos");
-                JOptionPane.showMessageDialog(null, "Tentativas restantes: " + (3 - tentativas));
+        JLabel labelSenha = new JLabel("Senha:");
+        labelSenha.setBounds(20,60,80,25);
+        janela.add(labelSenha);
+
+        JPasswordField campoSenha = new JPasswordField();
+        campoSenha.setBounds(100,60,150,25);
+        janela.add(campoSenha);
+
+        JButton botaoLogin = new JButton("Entrar");
+        botaoLogin.setBounds(40,110,90,30);
+        janela.add(botaoLogin);
+
+        JButton botaoCancelar = new JButton("Cancelar");
+        botaoCancelar.setBounds(150,110,100,30);
+        janela.add(botaoCancelar);
+
+        botaoLogin.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+
+                String usuario = campoUsuario.getText();
+                String senha = new String(campoSenha.getPassword());
+
+                if(usuario.equals("admin") && senha.equals("1234")) {
+
+                    JOptionPane.showMessageDialog(null,"Acesso Permitido");
+
+                } else {
+
+                    tentativas++;
+
+                    JOptionPane.showMessageDialog(null,
+                            "Login ou senha incorretos\nTentativas restantes: " + (3 - tentativas));
+
+                    if(tentativas == 3) {
+
+                        JOptionPane.showMessageDialog(null,"Sistema Bloqueado");
+                        System.exit(0);
+                    }
+                }
             }
-        }
+        });
 
-        if (tentativas == 3) {
-        	JOptionPane.showMessageDialog(null, "Sistema bloqueado");
-        }
+        botaoCancelar.addActionListener(new ActionListener() {
 
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        janela.setVisible(true);
     }
 }
